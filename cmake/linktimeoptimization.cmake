69da2c0d2f126c73f11c6538e294f3a5a81c99e9
Grace H <grace@hypnovir.us>
2019-08-27 19:21:11 -0700

    Do LTO for minimum size optimization.

--- cmake/linktimeoptimization.cmake
+++ cmake/linktimeoptimization.cmake
@@ -1,7 +1,7 @@
 include(CheckIPOSupported)
 check_ipo_supported(RESULT ipo_is_supported OUTPUT error)
 
-if( ipo_is_supported AND CMAKE_BUILD_TYPE MATCHES Release)
+if( ipo_is_supported AND (CMAKE_BUILD_TYPE MATCHES Release) OR (CMAKE_BUILD_TYPE MATCHES MinSizeRel))
 	message(STATUS "IPO / LTO enabled")
 	set(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE)
 else()
