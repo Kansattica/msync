addaae9effdbd0afe76845640eb7e8cf81ff61f3
Grace H <grace@hypnovir.us>
2019-08-29 15:28:16 -0700

    Add frozen sets.

--- console/msync.cpp
+++ console/msync.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <array>
 #include "../lib/common/net.hpp"
-#include "../lib/sync/recv.hpp"
+#include "../lib/parse/inoutbox.hpp"
 
 int main(int argc, char *argv[])
 {
 	std::cout << "Sup, nerds. Welcome to Mastosync.\n";
-	std::cout << read_url() << std::endl;
+	//getgoodstuff(read_url("https://cybre.space/api/v1/instance"));
 
-	getgoodstuff();
+	std::array<int, 5> nums{4, 100, 2, 300, -5};
+
+	for (auto v : nums)
+	{
+		std::cout << "The number " << v << " is " << (is_good(v) ? "good" : "bad") << ".\n";
+	}
 }
