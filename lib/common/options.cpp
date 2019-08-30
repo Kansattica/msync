01255cf26e1e3e8756fbeb4e68b994c66b6b0d45
Grace H <grace@hypnovir.us>
2019-08-30 00:28:13 -0700

    Flesh out the options object a little more.

--- lib/common/options.cpp
+++ lib/common/options.cpp
@@ -3,11 +3,21 @@
 
 struct global_options {
     bool verbose;
+
+    std::vector<std::string> accounts_to_sync;
+    std::vector<std::string> lists_to_sync;
 };
 
 struct user_options {
+    // including instance name, like name@instance.social
     std::string account_name;
-    std::vector<std::string> favorites;
+    std::string access_token;
 
+    std::vector<std::string> ids_to_favorite;
+    std::vector<std::string> ids_to_boost;
+    std::vector<std::string> filenames_to_post;
 
+    bool sync_home;
+    bool sync_dms;
+    bool sync_notifications;
 };
