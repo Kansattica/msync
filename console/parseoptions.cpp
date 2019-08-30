de1c0bfd0d11248c6810e3ba9ab3d3dd370150e1
Grace H <grace@hypnovir.us>
2019-08-30 01:43:48 -0700

    Let's try this with the options.

--- console/parseoptions.cpp
+++ console/parseoptions.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 
+#include "../lib/common/options.hpp"
 #include "parseoptions.hpp"
 
 std::vector<int> parse(int argc, char **argv)
@@ -10,9 +11,12 @@ std::vector<int> parse(int argc, char **argv)
     {
         TCLAP::CmdLine cmd("This is msync", ' ', "0.1");
         TCLAP::MultiArg<int> numArg("n", "num", "Number to check", false, "integer");
+        TCLAP::SwitchArg verboseArg("v", "verbose", "Produce more output", false);
         cmd.add(numArg);
+        cmd.add(verboseArg);
         cmd.parse(argc, argv);
 
+        options.verbose = verboseArg.getValue();
         return numArg.getValue();
     }
     catch (const TCLAP::ArgException &e)
