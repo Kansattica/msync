0159d01300b41eb8b261814141c45474d28395f6
Grace H <grace@hypnovir.us>
2019-08-30 16:29:12 -0700

    Try detecting the right filesystem with preprocessor magic

--- lib/common/options.cpp
+++ lib/common/options.cpp
@@ -3,7 +3,18 @@
 #include <constants.hpp>
 
 #include <vector>
+
+#ifdef __cpp_lib_filesystem
 #include <filesystem>
+using fs = std::filesystem;
+#warning "Regular filesystem found."
+#elif __cpp_lib_experimental_filesystem
+#include <experimental/filesystem>
+using fs = std::experimental::filesystem;
+#warning "Experimental filesystem found."
+#else
+#error "no filesystem support ='("
+#endif
 
 #include <whereami.h>
 
@@ -11,7 +22,7 @@ global_options options;
 
 void set_locations()
 {
-    options.current_working_directory = std::filesystem::current_path();
+    options.current_working_directory = fs::current_path();
 
     // see https://github.com/gpakosz/whereami
     const int length = wai_getModulePath(nullptr, 0, nullptr);
@@ -21,7 +32,7 @@ void set_locations()
     int dirname_length;
     wai_getExecutablePath(path.get(), length, &dirname_length);
     path[dirname_length + 1] = '\0';
-    options.executable_location = std::filesystem::path(path.get());
+    options.executable_location = fs::path(path.get());
 }
 
 std::optional<std::string> account_exists(std::string name)
