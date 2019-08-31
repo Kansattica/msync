8d708bf03566b1f4965531721d2478a1d1d978b8
Grace H <grace@hypnovir.us>
2019-08-30 21:03:48 -0700

    Better help, make help text use more of the screen.

--- console/msync.cpp
+++ console/msync.cpp
@@ -13,6 +13,8 @@ int main(int argc, char *argv[])
 	std::cout << "Sup, nerds. Welcome to Mastosync.\n";
 	//getgoodstuff(read_url("https://cybre.space/api/v1/instance"));
 
+	parse(argc, argv);
+
 	if (options.verbose)
 	{
 		std::cout << "I think that my executable is in " << options.executable_location << "\nand my current working directory is " << options.current_working_directory << std::endl;
--- console/parseoptions.cpp
+++ console/parseoptions.cpp
@@ -3,19 +3,31 @@
 #include <string>
 
 #include "../lib/common/options.hpp"
+#include "wideroutput.hpp"
 #include "parseoptions.hpp"
+
+const std::string helpmessage = R"(msync is a command line utility for synchronizing with a Mastodon API-compatible server.
+New account names must be fully specified, like: GoddessGrace@goodchristian.website.
+Account names for accounts that you have logged into msync with can be shortened as long as it's unambiguous.
+(for example, if you have GoddessGrace@goodchristian.website and GoodGrace@another.website, you could select them with -a God or -a Goo, respectively)
+)";
+
+TCLAP::WiderOutput fixedOutput {100};
 
 void parse(int argc, char **argv)
 {
     try
     {
         set_locations();
-        TCLAP::CmdLine cmd("This is msync", ' ', "0.1");
+        TCLAP::CmdLine cmd(helpmessage, ' ', "0.1");
         TCLAP::ValueArg<int> retryArg("r", "retries", "How many times to retry a failed operation", false, 3, "integer");
-        TCLAP::MultiArg<std::string> accountsArg("r", "retries", "How many times to retry a failed operation", false, "account name");
+        TCLAP::MultiArg<std::string> accountsArg("a", "account", "The account or accounts to operate on.", false, "account name");
         TCLAP::SwitchArg verboseArg("v", "verbose", "Produce more output", false);
         cmd.add(retryArg);
         cmd.add(verboseArg);
+        cmd.add(accountsArg);
+
+        cmd.setOutput(&fixedOutput);
         cmd.parse(argc, argv);
 
         options.verbose = verboseArg.getValue();
@@ -27,3 +39,4 @@ void parse(int argc, char **argv)
         std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
     }
 }
+
