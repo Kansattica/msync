9eb6a322a2cbd20b88f3d683da6a82daaa7be33d
Grace H <grace@hypnovir.us>
2019-08-30 21:41:03 -0700

    Clang still wants things experimental, I guess.

---
+++ lib/common/filesystem.hpp
@@ -0,0 +1,12 @@
+#ifndef _FS_HPP_
+#define _FS_HPP_
+
+#ifndef __clang__
+#include <filesystem>
+namespace fs = std::filesystem;
+#else
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+#endif
+
+#endif 
