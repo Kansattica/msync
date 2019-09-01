17060e77c6ecb4767a4a2567a3177b0f55aa25f7
Grace H <Grace@hypnovir.us>
2019-09-01 16:26:42 -0700

    Move parsing options into its own library.

--- console/optionparsing/parseoptions.cpp
+++ console/optionparsing/parseoptions.cpp
@@ -2,8 +2,9 @@
 #include <frozen/set.h>
 #include <iostream>
 #include <string>
+#include <vector>
 
-#include "../lib/options/options.hpp"
+#include "../../lib/options/options.hpp"
 #include "wideroutput.hpp"
 #include "termwidth.hpp"
 #include "parseoptions.hpp"
@@ -15,32 +16,41 @@ Account names for accounts that you have logged into msync with can be shortened
 )";
 
 const int columns = term_width();
-TCLAP::WiderOutput fixedOutput {columns};
+
+// tclap isn't very const correct, huh
+std::vector<std::string> validOptions {"accesstoken", "clientsecret", "authcode", "username", "password"};
 
 void parse(int argc, char **argv)
 {
+    TCLAP::ValuesConstraint<std::string> allowedOptions(validOptions);
     try
     {
         TCLAP::CmdLine cmd(helpmessage, ' ', "0.1");
         TCLAP::ValueArg<int> retry("r", "retries", "How many times to retry a failed operation.", false, 3, "integer", cmd);
-        TCLAP::ValueArg<std::string> set("", "set", "The name of the option to set for the given account. Acceptable values are 'accesstoken', 'clientsecret', 'authcode', 'username', 'password'", false, "", "option name", cmd);
+        TCLAP::ValueArg<std::string> option("", "option", "The name of the option to read or set for the given account.", false, "", &allowedOptions, cmd);
+        TCLAP::ValueArg<std::string> setOption("", "set", "What to set the named option to.", false, "", "option value", cmd);
         TCLAP::MultiArg<std::string> accounts("a", "account", "The account or accounts to operate on.", false, "account name", cmd);
         TCLAP::SwitchArg verbose("v", "verbose", "Produce more output.", cmd, false);
 
-        cmd.setOutput(&fixedOutput);
+        TCLAP::WiderOutput wideOut{columns};
+        cmd.setOutput(&wideOut);
+
         cmd.parse(argc, argv);
 
         options.verbose = verbose.getValue();
         options.retries = retry.getValue();
 
         if (options.verbose)
-        {
             std::cout << "I detected your terminal width to be " << columns << " columns wide.\n";
-        }
+
+        if (setOption.isSet() && !option.isSet())
+            std::cerr << "If you're going to use --set, you have to provide an option to set.";
+
     }
     catch (const TCLAP::ArgException &e)
     {
         std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
     }
 }
+
 
