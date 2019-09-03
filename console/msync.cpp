e8f3bdd774ff070cbe55f4a8ef3c660b716d855a
Grace H <grace@hypnovir.us>
2019-09-03 13:11:39 -0700

    Clean up main() a little.

--- console/msync.cpp
+++ console/msync.cpp
@@ -7,11 +7,10 @@
 int main(int argc, char *argv[])
 {
 	std::cout << "Sup, nerds. Welcome to Mastosync.\n";
-	//getgoodstuff(read_url("https://cybre.space/api/v1/instance"));
 
 	parse(argc, argv);
 
 	PrintLogger<logtype::verbose> pl;
-	pl << "My executable is in " << options.executable_location << '\n';
-	pl << "My current working directory is " << options.current_working_directory << '\n';
+	pl << "Looking for options in " << options.executable_location << '\n';
+	pl << "My current working directory for logs and arguments is " << options.current_working_directory << '\n';
 }
