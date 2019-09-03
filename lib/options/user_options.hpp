db898cf117bf92bddd97fce4e751fd76c61b3e40
Grace H <grace@hypnovir.us>
2019-09-03 09:50:03 -0700

    First step towards tristate sync logic.

--- lib/options/user_options.hpp
+++ lib/options/user_options.hpp
@@ -13,12 +13,29 @@ struct user_options
 
     std::vector<std::string> lists_to_pull;
 
-    bool pull_profile;
-    bool push_profile;
-
-    bool pull_home;
-    bool pull_dms;
-    bool pull_notifications;
+    sync_settings pull_home = sync_settings::newest_first;
+    sync_settings pull_dms = sync_settings::oldest_first;
+    sync_settings pull_notifications = sync_settings::oldest_first;
 };
 
-enum class user_option {show, accesstoken, username, password, clientsecret, addlist, removelist, home, dms, notifications, newaccount};
+enum class user_option
+{
+    show,
+    accesstoken,
+    username,
+    password,
+    clientsecret,
+    addlist,
+    removelist,
+    home,
+    dms,
+    notifications,
+    newaccount
+};
+
+enum class sync_settings
+{
+    off,
+    newest_first,
+    oldest_first
+};
