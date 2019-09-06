55a738d2cacbfbd1a65f8e00a0f2bb861a5b38e4
Grace H <grace@hypnovir.us>
2019-09-06 14:48:49 -0700

    get_options now returns const pointer to const string.

--- lib/options/user_options.cpp
+++ lib/options/user_options.cpp
@@ -3,27 +3,21 @@
 #include <array>
 
 const auto optionsToKeys =
-    std::array<std::string, static_cast<int>(user_option::notifications) + 1>(
-        {"account_name",
-         "instance_url",
-         "access_token",
-         "username",
-         "password",
-         "client_secret",
-         "pull_home",
-         "pull_dms",
-         "pull_notifications"});
+    std::array<const std::string,
+               static_cast<int>(user_option::notifications) + 1>(
+        {"account_name", "instance_url", "access_token", "username", "password",
+         "client_secret", "pull_home", "pull_dms", "pull_notifications"});
 
-std::optional<std::string_view> user_options::get_option(user_option opt)
-{
-    auto val = backing.parsed_options.find(optionsToKeys[static_cast<int>(opt)]);
-    if (val == backing.parsed_options.end())
-        return {};
+const std::string* const user_options::get_option(user_option opt) {
+  const auto val =
+      backing.parsed_options.find(optionsToKeys[static_cast<int>(opt)]);
+  if (val == backing.parsed_options.end())
+    return nullptr;
 
-    return val->second;
+  return &val->second;
 }
 
-void user_options::set_option(user_option opt, std::string value)
-{
-    backing.parsed_options[optionsToKeys[static_cast<int>(opt)]] = value;
+void user_options::set_option(user_option opt, std::string&& value) {
+  backing.parsed_options.insert_or_assign(optionsToKeys[static_cast<int>(opt)],
+                                          std::forward<std::string>(value));
 }
--- lib/options/user_options.hpp
+++ lib/options/user_options.hpp
@@ -1,46 +1,32 @@
 #include <string>
-#include <string_view>
-#include <optional>
 
 #include "filesystem.hpp"
 
 #include "option_file.hpp"
 
-enum class list_operations
-{
-    add,
-    remove,
-    clear
+enum class list_operations { add, remove, clear };
+
+enum class sync_settings { off, newest_first, oldest_first };
+
+enum class user_option {
+  accountname,
+  instanceurl,
+  accesstoken,
+  username,
+  password,
+  clientsecret,
+  home,
+  dms,
+  notifications,
 };
 
-enum class sync_settings
-{
-    off,
-    newest_first,
-    oldest_first
-};
-
-enum class user_option
-{
-    accountname,
-    instanceurl,
-    accesstoken,
-    username,
-    password,
-    clientsecret,
-    home,
-    dms,
-    notifications,
-};
-
-struct user_options
-{
+struct user_options {
 public:
-    user_options(fs::path toread) : backing(toread){};
+  user_options(fs::path toread) : backing(toread){};
 
-    std::optional<std::string_view> get_option(user_option toget);
-    void set_option(user_option toset, std::string value);
+  const std::string* const get_option(user_option toget);
+  void set_option(user_option toset, std::string&& value);
 
 private:
-    option_file backing;
+  option_file backing;
 };
