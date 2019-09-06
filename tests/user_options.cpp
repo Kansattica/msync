9d0c059aec73e2f98e7119c5458913f4431fd92f
Grace H <Grace@hypnovir.us>
2019-09-05 17:10:43 -0700

    Test that user_options also makes those .bak files.

--- tests/user_options.cpp
+++ tests/user_options.cpp
@@ -12,6 +12,8 @@ SCENARIO("User_options reads from a file when created", "[user_options]")
     {
         const fs::path filepath("someuser.test");
         test_file fi(filepath);
+        const fs::path backup("someuser.test.bak");
+        test_file fibak(backup);
 
         {
             std::ofstream maketest(filepath);
@@ -30,9 +32,14 @@ SCENARIO("User_options reads from a file when created", "[user_options]")
                 REQUIRE(opt.get_option(user_option::instanceurl).value() == "website.egg");
             }
 
-            THEN("Empty fields are handled gracefully.")
+            THEN("empty fields are handled gracefully.")
             {
                 REQUIRE_FALSE(opt.get_option(user_option::accesstoken).has_value());
+            }
+
+            THEN("No .bak file is created.")
+            {
+                REQUIRE_FALSE(fs::exists(backup));
             }
         }
     }
@@ -44,6 +51,10 @@ SCENARIO("User_options saves changes back to its file", "[user_options]")
     {
         const fs::path filepath("someuser.test");
         test_file fi(filepath);
+
+        fs::path backup(filepath);
+        backup += ".bak";
+        test_file fibak(backup);
 
         {
             std::ofstream maketest(filepath);
@@ -65,6 +76,17 @@ SCENARIO("User_options saves changes back to its file", "[user_options]")
                 REQUIRE(newopt.get_option(user_option::accountname).value() == "someoneelse");
                 REQUIRE(newopt.get_option(user_option::instanceurl).value() == "website.egg");
                 REQUIRE_FALSE(newopt.get_option(user_option::accesstoken).has_value());
+            }
+
+            THEN ("a .bak file with the original information should be created.")
+            {
+                REQUIRE(fs::exists(backup));
+
+                auto lines = read_lines(backup);
+
+                REQUIRE(lines.size() == 2);
+                REQUIRE(lines[0] == "account_name=sometester");
+                REQUIRE(lines[1] == "instance_url=website.egg");
             }
         }
     }
@@ -100,6 +122,13 @@ SCENARIO("An empty user_options writes to a file when destroyed", "[user_options
                 REQUIRE(lines.size() == 2);
                 REQUIRE(lines[0] == "account_name=somejerk");
                 REQUIRE(lines[1] == "instance_url=rude.website");
+            }
+
+            THEN("No .bak file is created.")
+            {
+                fs::path backup(filepath);
+                backup += ".bak";
+                REQUIRE_FALSE(fs::exists(backup));
             }
         }
     }
