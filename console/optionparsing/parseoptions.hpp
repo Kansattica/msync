519c3f9205d4fdc36ce4c393ef85d6bf5fa410ff
Grace H <Grace@hypnovir.us>
2019-09-05 23:53:53 -0700

    Fix parseoptions API and test it.

--- console/optionparsing/parseoptions.hpp
+++ console/optionparsing/parseoptions.hpp
@@ -1,2 +1,28 @@
+#include <string>
+#include "../../lib/options/options.hpp"
 
-void parse(int argc, char **argv);
+enum class mode
+{
+    newuser,
+    showopt,
+    config,
+    configsync,
+    configlist,
+    sync,
+    gen,
+    queue,
+    help
+};
+
+struct parse_result
+{
+    bool okay;
+    mode selected = mode::help;
+    user_option toset;
+    sync_settings syncset;
+    list_operations listops;
+    std::string optionval;
+    std::string account;
+};
+
+parse_result parse(int argc, char **argv, bool silent = true);
