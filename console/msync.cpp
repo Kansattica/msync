23fbe63063f92726b78adfda266e361e49f8ca1a
Grace H <Grace@hypnovir.us>
2019-09-08 14:58:26 -0700

    Log program start and stop, switch on selected mode.

--- console/msync.cpp
+++ console/msync.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <printlogger.hpp>
 
 #include "optionparsing/parseoptions.hpp"
@@ -5,9 +7,18 @@
 int main(int argc, char *argv[])
 {
 	std::cout << "Sup, nerds. Welcome to Mastosync.\n";
+	PrintLogger<logtype::fileonly> pl;
+	pl << "--- Mastosync started ---\n";
 
 	auto parsed = parse(argc, argv, false);
+	
+	switch (parsed.selected)
+	{
+		case mode::newuser:
 
+		default:
+			std::cout << "[option not implemented]\n";
+	}
 
-
+	pl << "--- Mastosync finished normally ---\n";
 }
--- lib/options/global_options.hpp
+++ lib/options/global_options.hpp
@@ -1,15 +1,17 @@
 #include "filesystem.hpp"
 #include "user_options.hpp"
 
+#include <unordered_map>
+#include <string>
+
 struct global_options
 {
     public:
         bool verbose = false;
-        int retries = 3;
         fs::path executable_location = get_exe_location();
         fs::path current_working_directory = fs::current_path();
 
-        std::vector<user_options> accounts;
+        std::unordered_map<std::string, user_options> accounts;
 
     private:
         fs::path get_exe_location();
