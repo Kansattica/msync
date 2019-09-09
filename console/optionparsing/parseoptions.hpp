b3b630da9121a98f17ccfb558ed6ec07e856e740
Grace H <Grace@hypnovir.us>
2019-09-08 22:13:37 -0700

    Don't play around with unions right now.

--- console/optionparsing/parseoptions.hpp
+++ console/optionparsing/parseoptions.hpp
@@ -20,13 +20,12 @@ struct parse_result
     bool okay;
     mode selected = mode::help;
     user_option toset;
-    union { //only one or the other will be set
-        sync_settings syncset;
-        list_operations listops;
-        sync_options syncopts;
-    }; 
+    // maybe later, do a union or variant for the mutually exclusive ones
+    sync_settings syncset;
+    list_operations listops;
+    sync_options syncopts;
     std::string optionval;
     std::string account;
 };
 
-parse_result parse(int argc, const char *argv[], bool silent = true);
+parse_result parse(int argc, const char* argv[], bool silent = true);
