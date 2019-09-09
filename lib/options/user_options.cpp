83014a2dd2fb355a371a9a9aebcb8aab8a75d751
Grace H <Grace@hypnovir.us>
2019-09-08 23:14:09 -0700

    Fix formatting, maximize constness

--- lib/options/user_options.cpp
+++ lib/options/user_options.cpp
@@ -2,22 +2,26 @@
 
 #include <array>
 
+using namespace std::string_literals;
+
 const auto optionsToKeys =
     std::array<const std::string,
                static_cast<int>(user_option::notifications) + 1>(
-        {"account_name", "instance_url", "access_token", "username", "password",
-         "client_secret", "pull_home", "pull_dms", "pull_notifications"});
+        {"account_name"s, "instance_url"s, "access_token"s, "username"s, "password"s,
+         "client_secret"s, "pull_home"s, "pull_dms"s, "pull_notifications"s});
 
-const std::string* const user_options::get_option(user_option opt) const {
-  const auto val =
-      backing.parsed_options.find(optionsToKeys[static_cast<int>(opt)]);
-  if (val == backing.parsed_options.end())
-    return nullptr;
+const std::string* const user_options::get_option(const user_option opt) const
+{
+    const auto val =
+        backing.parsed_options.find(optionsToKeys[static_cast<int>(opt)]);
+    if (val == backing.parsed_options.end())
+        return nullptr;
 
-  return &val->second;
+    return &val->second;
 }
 
-void user_options::set_option(user_option opt, std::string&& value) {
-  backing.parsed_options.insert_or_assign(optionsToKeys[static_cast<int>(opt)],
-                                          std::forward<std::string>(value));
+void user_options::set_option(user_option opt, std::string&& value)
+{
+    backing.parsed_options.insert_or_assign(optionsToKeys[static_cast<int>(opt)],
+                                            std::forward<std::string>(value));
 }
