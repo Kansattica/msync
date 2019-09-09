62156452660bf5cb29a40d080f326169c81f44a1
Grace H <grace@hypnovir.us>
2019-09-09 09:59:16 -0700

    Same here, making the pointer itself const doesn't mean much.

--- tests/options.cpp
+++ tests/options.cpp
@@ -1,8 +1,7 @@
-#include <catch2/catch.hpp>
 #include "test_helpers.hpp"
+#include <catch2/catch.hpp>
 
 #include "../lib/options/options.hpp"
-
 
 SCENARIO("select_account selects exactly one account.", "[options]")
 {
@@ -10,11 +9,11 @@ SCENARIO("select_account selects exactly one account.", "[options]")
     {
         options.accounts.clear();
 
-        WHEN ("select_account is given an empty string to search on")
+        WHEN("select_account is given an empty string to search on")
         {
             auto account = select_account("");
 
-            THEN ("an error message is returned")
+            THEN("an error message is returned")
             {
                 REQUIRE(std::holds_alternative<const std::string>(account));
             }
@@ -25,11 +24,11 @@ SCENARIO("select_account selects exactly one account.", "[options]")
             }
         }
 
-        WHEN ("select_account is given a non-empty string to search on")
+        WHEN("select_account is given a non-empty string to search on")
         {
             auto account = select_account("coolperson");
 
-            THEN ("an error message is returned")
+            THEN("an error message is returned")
             {
                 REQUIRE(std::holds_alternative<const std::string>(account));
             }
@@ -43,37 +42,37 @@ SCENARIO("select_account selects exactly one account.", "[options]")
 
     GIVEN("An account_options with one entry")
     {
-        const fs::path optiontestfile {"regulartest"};
-        test_file fi {optiontestfile};
+        const fs::path optiontestfile{"regulartest"};
+        test_file fi{optiontestfile};
 
         options.accounts.clear();
         options.accounts.insert({"someaccount@website.com", user_options(optiontestfile)});
 
-        WHEN ("select_account is given an empty string to search on")
+        WHEN("select_account is given an empty string to search on")
         {
             auto account = select_account("");
 
-            THEN ("a user_options is returned.")
+            THEN("a user_options is returned.")
             {
-                REQUIRE(std::holds_alternative<const user_options* const>(account));
+                REQUIRE(std::holds_alternative<const user_options*>(account));
             }
         }
 
-        WHEN ("select_account is given a non-empty string to search on that's a valid prefix")
+        WHEN("select_account is given a non-empty string to search on that's a valid prefix")
         {
             auto account = select_account("some");
 
-            THEN ("a user_options is returned.")
+            THEN("a user_options is returned.")
             {
-                REQUIRE(std::holds_alternative<const user_options* const>(account));
+                REQUIRE(std::holds_alternative<const user_options*>(account));
             }
         }
 
-        WHEN ("select_account is given a non-empty string to search on that's not a valid prefix")
+        WHEN("select_account is given a non-empty string to search on that's not a valid prefix")
         {
             auto account = select_account("bad");
 
-            THEN ("an error message is returned")
+            THEN("an error message is returned")
             {
                 REQUIRE(std::holds_alternative<const std::string>(account));
             }
@@ -87,25 +86,25 @@ SCENARIO("select_account selects exactly one account.", "[options]")
 
     GIVEN("An account_options with two entries")
     {
-        const fs::path optiontestfile {"regulartest"};
-        test_file fi {optiontestfile};
-        const fs::path anotheroptiontestfile {"regulartestguy"};
-        test_file anotherfi {anotheroptiontestfile};
+        const fs::path optiontestfile{"regulartest"};
+        test_file fi{optiontestfile};
+        const fs::path anotheroptiontestfile{"regulartestguy"};
+        test_file anotherfi{anotheroptiontestfile};
 
         options.accounts.clear();
         options.accounts.insert({"someaccount@website.com", user_options(optiontestfile)});
         options.accounts.insert({"someotheraccount@place2.egg", user_options(optiontestfile)});
 
-        for (auto &pair : options.accounts)
+        for (auto& pair : options.accounts)
         {
             pair.second.set_option(user_option::accountname, std::string(pair.first)); //make a copy because you can't mutate keys, and user_option might mutate
         }
 
-        WHEN ("select_account is given an empty string to search on")
+        WHEN("select_account is given an empty string to search on")
         {
             auto account = select_account("");
 
-            THEN ("an error message is returned")
+            THEN("an error message is returned")
             {
                 REQUIRE(std::holds_alternative<const std::string>(account));
             }
@@ -116,11 +115,11 @@ SCENARIO("select_account selects exactly one account.", "[options]")
             }
         }
 
-        WHEN ("select_account is given a non-empty string to search on that's an ambiguous prefix")
+        WHEN("select_account is given a non-empty string to search on that's an ambiguous prefix")
         {
             auto account = select_account("some");
 
-            THEN ("an error message is returned")
+            THEN("an error message is returned")
             {
                 REQUIRE(std::holds_alternative<const std::string>(account));
             }
@@ -131,27 +130,27 @@ SCENARIO("select_account selects exactly one account.", "[options]")
             }
         }
 
-        WHEN ("select_account is given a non-empty string to search on that's an unambiguous prefix")
+        WHEN("select_account is given a non-empty string to search on that's an unambiguous prefix")
         {
             auto account = select_account("someother");
 
-            THEN ("a user_options is returned.")
+            THEN("a user_options is returned.")
             {
-                REQUIRE(std::holds_alternative<const user_options* const>(account));
+                REQUIRE(std::holds_alternative<const user_options*>(account));
             }
 
-            THEN ("the user_options is the correct one.")
+            THEN("the user_options is the correct one.")
             {
-                const std::string account_name = *std::get<const user_options* const>(account)->get_option(user_option::accountname);
+                const std::string account_name = *std::get<const user_options*>(account)->get_option(user_option::accountname);
                 REQUIRE(account_name == "someotheraccount@place2.egg");
             }
         }
 
-        WHEN ("select_account is given a non-empty string to search on that's not a valid prefix")
+        WHEN("select_account is given a non-empty string to search on that's not a valid prefix")
         {
             auto account = select_account("bad");
 
-            THEN ("an error message is returned")
+            THEN("an error message is returned")
             {
                 REQUIRE(std::holds_alternative<const std::string>(account));
             }
