7fcf5801281db346cf8aa03dec2783a56596e095
Grace H <grace@hypnovir.us>
2019-09-09 13:14:59 -0700

    Move the global global_options to the global_options.

--- lib/options/global_options.hpp
+++ lib/options/global_options.hpp
@@ -4,21 +4,22 @@
 #include "filesystem.hpp"
 #include "user_options.hpp"
 
-#include <unordered_map>
 #include <string>
+#include <unordered_map>
 
 struct global_options
 {
-    public:
-        bool verbose = false;
-        const fs::path executable_location = get_exe_location();
-        const fs::path current_working_directory = fs::current_path();
+public:
+    bool verbose = false;
+    const fs::path executable_location = get_exe_location();
+    const fs::path current_working_directory = fs::current_path();
 
-        std::unordered_map<std::string, user_options> accounts = read_accounts();
+    std::unordered_map<std::string, user_options> accounts = read_accounts();
 
-    private:
-        fs::path get_exe_location();
-        std::unordered_map<std::string, user_options> read_accounts();
+private:
+    fs::path get_exe_location();
+    std::unordered_map<std::string, user_options> read_accounts();
 };
 
+extern global_options options;
 #endif
