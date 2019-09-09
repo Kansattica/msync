e49de15261edef951ec0c622f44bbab6cae1aaad
Grace H <grace@hypnovir.us>
2019-09-09 09:56:08 -0700

    Returning a const pointer doesn't make much sense.

--- lib/options/user_options.hpp
+++ lib/options/user_options.hpp
@@ -7,30 +7,42 @@
 
 #include "option_file.hpp"
 
-enum class list_operations { add, remove, clear };
-
-enum class sync_settings { off, newest_first, oldest_first };
-
-enum class user_option {
-  accountname,
-  instanceurl,
-  accesstoken,
-  username,
-  password,
-  clientsecret,
-  home,
-  dms,
-  notifications,
+enum class list_operations
+{
+    add,
+    remove,
+    clear
 };
 
-struct user_options {
-public:
-  user_options(fs::path toread) : backing(toread){};
+enum class sync_settings
+{
+    off,
+    newest_first,
+    oldest_first
+};
 
-  const std::string* const get_option(user_option toget) const;
-  void set_option(user_option toset, std::string&& value);
+enum class user_option
+{
+    accountname,
+    instanceurl,
+    accesstoken,
+    username,
+    password,
+    clientsecret,
+    home,
+    dms,
+    notifications,
+};
+
+struct user_options
+{
+public:
+    user_options(fs::path toread) : backing(toread){};
+
+    const std::string* get_option(user_option toget) const;
+    void set_option(user_option toset, std::string&& value);
 
 private:
-  option_file backing;
+    option_file backing;
 };
 #endif
