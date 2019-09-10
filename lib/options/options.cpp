f1f5765aaa7f20e0402df6b969dca350ad286e73
Grace H <grace@hypnovir.us>
2019-09-10 15:39:10 -0700

    parse_account_name should get rid of the scheme.

--- lib/options/options.cpp
+++ lib/options/options.cpp
@@ -1,14 +1,10 @@
 #include "options.hpp"
-#include "user_options.hpp"
 
-#include <algorithm>
 #include <regex>
-
-#include <print_logger.hpp>
 
 std::optional<parsed_account> parse_account_name(const std::string& name)
 {
-    const static std::regex account_name{"@?([_a-z0-9]+)@([a-z0-9-]+\\.[a-z0-9-]+(?:\\.[a-z0-9-]+)?)[, =/\\\\?]*$", std::regex::ECMAScript | std::regex::icase};
+    const static std::regex account_name{"@?([_a-z0-9]+)@(?:https?://)?([a-z0-9-]+\\.[a-z0-9-]+(?:\\.[a-z0-9-]+)?)[, =/\\\\?]*$", std::regex::ECMAScript | std::regex::icase};
 
     std::smatch results;
     if (std::regex_match(name, results, account_name))
