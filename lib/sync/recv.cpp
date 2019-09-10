6816a9077430834b4439355610b9aecfb91eb414
Grace H <grace@hypnovir.us>
2019-09-10 16:01:52 -0700

    No more dead code in recv

--- lib/sync/recv.cpp
+++ lib/sync/recv.cpp
@@ -1,13 +1,1 @@
 #include "recv.hpp"
-#include <nlohmann/json.hpp>
-#include <iostream>
-#include <iomanip>
-
-using json = nlohmann::json;
-
-void getgoodstuff(std::string jsonish)
-{
-	json jerry = json::parse(jsonish);
-
-	std::cout << std::setw(4) << jerry << std::endl;
-}
