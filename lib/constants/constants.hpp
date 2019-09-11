6b3b4180b4dda6487080733e7ec7c90a7321dc09
Grace H <Grace@hypnovir.us>
2019-09-11 00:46:22 -0700

    maybe store lists in a lists.config file?

--- lib/constants/constants.hpp
+++ lib/constants/constants.hpp
@@ -6,5 +6,6 @@
 // inline should make sure there's only one between all the files
 inline const std::string Account_Directory{"accounts"};
 inline const std::string User_Options_Filename{"user.config"};
+inline const std::string List_Options_Filename{"lists.config"};
 
 #endif
