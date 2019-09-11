19ea8d4a071b37e61b29f8e979cdfbc21ea3ebfc
Grace H <grace@hypnovir.us>
2019-09-11 12:34:27 -0700

    Add getting sync settings from user_options

--- lib/options/option_enums.cpp
+++ lib/options/option_enums.cpp
@@ -2,33 +2,33 @@
 
 #include <msync_exception.hpp>
 
-template<>
-list_operations parse_enum<list_operations>(char first)
+template <>
+list_operations parse_enum<list_operations>(const char first)
 {
     switch (first)
     {
-        case 'a':
-            return list_operations::add;
-        case 'r':
-            return list_operations::remove;
-        case 'c':
-            return list_operations::clear;
+    case 'a':
+        return list_operations::add;
+    case 'r':
+        return list_operations::remove;
+    case 'c':
+        return list_operations::clear;
     }
 
     throw msync_exception("No list_operation starting with " + first);
 }
 
-template<>
-sync_settings parse_enum<sync_settings>(char first)
+template <>
+sync_settings parse_enum<sync_settings>(const char first)
 {
     switch (first)
     {
-        case 'd':
-            return sync_settings::dont_sync;
-        case 'n':
-            return sync_settings::newest_first;
-        case 'o':
-            return sync_settings::oldest_first;
+    case 'd':
+        return sync_settings::dont_sync;
+    case 'n':
+        return sync_settings::newest_first;
+    case 'o':
+        return sync_settings::oldest_first;
     }
 
     throw msync_exception("No sync_setting starting with " + first);
