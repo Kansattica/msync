ad900866f9e376e77a7caa2db56d122243c05ae6
Grace H <grace@hypnovir.us>
2019-10-15 10:22:08 -0700

    Some compilers won't convert a string_view to a path.

--- tests/test_helpers.hpp
+++ tests/test_helpers.hpp
@@ -6,12 +6,14 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <string_view>
 
 //ensures a file doesn't exist before and after each test run.
 struct test_file
 {
 public:
 	test_file(const char* name) : test_file(fs::path(name)) {};
+	test_file(const std::string_view name) : test_file(fs::path(name)) {};
     test_file(fs::path name) : filename(name), filenamebak(name)
     {
         filenamebak += ".bak";
