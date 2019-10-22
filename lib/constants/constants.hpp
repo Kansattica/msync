cb31e71cd52326e9197536b2d65f7ec74b35ca43
Grace H <grace@hypnovir.us>
2019-10-22 12:57:48 -0700

    Inline is unnecessary here, I think

--- lib/constants/constants.hpp
+++ lib/constants/constants.hpp
@@ -3,16 +3,15 @@
 
 #include <string_view>
 
-// inline should make sure there's only one between all the files
-inline constexpr std::string_view Account_Directory{"msync_accounts"};
+constexpr std::string_view Account_Directory{"msync_accounts"};
 
-inline constexpr std::string_view User_Options_Filename{"user.config"};
-inline constexpr std::string_view List_Options_Filename{"lists.config"};
+constexpr std::string_view User_Options_Filename{"user.config"};
+constexpr std::string_view List_Options_Filename{"lists.config"};
 
-inline constexpr std::string_view Fav_Queue_Filename{"fav.queue"};
-inline constexpr std::string_view Boost_Queue_Filename{"boost.queue"};
-inline constexpr std::string_view Post_Queue_Filename{"post.queue"};
+constexpr std::string_view Fav_Queue_Filename{"fav.queue"};
+constexpr std::string_view Boost_Queue_Filename{"boost.queue"};
+constexpr std::string_view Post_Queue_Filename{"post.queue"};
 
-inline constexpr std::string_view File_Queue_Directory{"queuedposts"};
+constexpr std::string_view File_Queue_Directory{"queuedposts"};
 
 #endif
