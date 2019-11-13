fcfe3320c27c59d398279bfab406d5f2dec1de3d
Grace H <grace@hypnovir.us>
2019-11-13 01:59:33 -0800

    Use __has_include to determine which filesystem library to grab.

--- lib/filesystem/filesystem.hpp
+++ lib/filesystem/filesystem.hpp
@@ -1,12 +1,14 @@
 #ifndef _FS_HPP_
 #define _FS_HPP_
 
-#ifndef __clang__
+#if __has_include(<filesystem>)
 #include <filesystem>
 namespace fs = std::filesystem;
-#else
+#elif __has_include(<experimental/filesystem>)
 #include <experimental/filesystem>
 namespace fs = std::experimental::filesystem;
+#else
+#error Could not find filesystem library in <filesystem> or <experimental/filesystem>
 #endif
 
 #endif 
