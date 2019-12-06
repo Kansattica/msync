4a890b74f059d9ae2b4bf03a3404f1da01462a82
Grace H <grace@hypnovir.us>
2019-12-06 11:18:39 -0800

    Add proper scopes for reading notifications and timelines.

--- console/newaccount.cpp
+++ console/newaccount.cpp
@@ -16,8 +16,8 @@
 
 using json = nlohmann::json;
 
-constexpr auto scopes = "write:favourites write:media write:statuses";
-constexpr auto urlscopes = "write:favourites%20write:media%20write:statuses";
+constexpr auto scopes = "write:favourites write:media write:statuses read:notifications read:statuses";
+constexpr auto urlscopes = "write:favourites%20write:media%20write:statuses%20read:notifications%20read:statuses";
 constexpr auto redirect_uri = "urn:ietf:wg:oauth:2.0:oob";
 
 void make_new_account(const std::string& accountname)
