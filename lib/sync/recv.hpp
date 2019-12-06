f4c72d79e6e61be97039809793da29f014a59be4
Grace H <grace@hypnovir.us>
2019-12-06 09:54:28 -0800

    First try at actually getting posts.

--- lib/sync/recv.hpp
+++ lib/sync/recv.hpp
@@ -5,6 +5,14 @@
 
 #include "../net_interface/net_interface.hpp"
 
+#include "../options/user_options.hpp"
+
+#include "../constants/constants.hpp"
+#include "../postlist/post_list.hpp"
+#include "../util/util.hpp"
+
+#include "sync_helpers.hpp"
+
 #include <string_view>
 
 template <typename get_posts>
@@ -15,21 +23,59 @@ public:
 	unsigned int max_requests = 10;
 	unsigned int per_call = 20;
 
-	recv_posts(get_posts& get_posts) : get(get_posts) {};
+	recv_posts(get_posts& get_posts) : download(get_posts) {};
 
-	void get(const std::string_view account)
+	void get(std::string_view account_name, user_options& account)
 	{
 		retries = set_default(retries, 3, "Number of retries cannot be zero or less. Resetting to 3.\n", pl());
 		max_requests = set_default(max_requests, 5, "Maximum requests cannot be zero or less. Resetting to 5.\n", pl());
 		per_call = set_default(per_call, 20, "Number of posts to get per call cannot be zero or less. Resetting to 20.\n", pl());
 
-		pl() << "Downloading notifications for " << account << '\n';
-		pl() << "Downloading the home timeline for " << account << '\n';
+		pl() << "Downloading notifications for " << account_name << '\n';
+		update_timeline<to_get::notifications>(account);
+		pl() << "Downloading the home timeline for " << account_name << '\n';
 	}
 
 private:
+	get_posts& download;
 
-	get_posts& get;
+	enum class to_get { notifications, home, lists };
+
+	struct timeline_parameters { user_option last_id_setting; user_option sync_setting; std::string_view route; std::string_view filename; };
+
+	template <to_get timeline>
+	constexpr timeline_parameters get_parameters() const
+	{
+		if constexpr (timeline == to_get::notifications)
+		{
+			return { user_option::last_notification_id, user_option::pull_notifications, notifications_route, Notifications_Filename }
+		}
+	}
+
+	template <to_get timeline>
+	void update_timeline(user_options& account)
+	{
+		constexpr auto params = get_parameters<timeline>();
+
+		const sync_settings sync_method = account.get_sync_option(params.sync_setting);
+
+		if (sync_method == sync_settings::dont_sync)
+			return;
+
+		const std::string& access_token = account.get_option(user_option::access_token);
+		const std::string* last_id = account.try_get_option(params.last_id_setting);
+
+		const std::string url = make_api_url(account.get_option(user_option::instance_url), params.route);
+
+		// if last_id isn't set, we just wanna get a bunch of posts from the server, newest first
+		// otherwise, start getting posts either starting from or stopping at last_id, depending on the sync_method
+
+		for (unsigned int i = 0; i < max_requests; i++)
+		{
+			auto posts = download(url, access_token, "", per_call);
+		}
+
+	}
 };
 
 #endif
---
+++ tests/recv.cpp
@@ -0,0 +1,4 @@
+#include <catch2/catch.hpp>
+
+#include "../lib/sync/recv.hpp"
+
