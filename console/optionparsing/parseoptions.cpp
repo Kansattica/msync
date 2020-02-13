56d29e8c884e0b6e61b78cc6e136c5323d3ac473
Grace H <grace@hypnovir.us>
2020-02-13 14:48:51 -0800

    Reorder gen arguments to something more reasonable.

--- console/optionparsing/parseoptions.cpp
+++ console/optionparsing/parseoptions.cpp
@@ -79,13 +79,13 @@ parse_result parse(const int argc, const char* argv[], const bool silent)
 
 	const auto genMode = (command("gen", "generate").set(ret.selected, mode::gen).doc("Generate a post template in the current folder. Edit this file afterwards to add a body.") &
 			(
+			 (option("-o", "--output") & value("filename", ret.gen_opt.filename)).doc("Specify an output file. Default is new_post."),
+			 (option("-c", "--content-warning", "--cw") & value("warning", ret.gen_opt.post.content_warning)).doc("Set a content warning (or subject) for the post."),
+			 (option("-p", "--privacy", "--visibility") & visibilities).doc("Set the post's visibility."),
 			 (option("-f", "--file", "--attach") & values(match::prefix_not("-"), "file path", ret.gen_opt.post.attachments)).doc("Attach these files to the post."),
 			 (option("-d", "--description") & values("file description", ret.gen_opt.post.descriptions)).doc("Associate this description with the corresponding file."),
-			 (option("-o", "--output") & value("filename", ret.gen_opt.filename)).doc("Specify an output file. Default is new_post."),
 			 (option("-r", "--reply-to") & value("reply_to", ret.gen_opt.post.reply_to_id)).doc("Reply to the specified post ID."),
-			 (option("-i", "--reply-id") & value("id", ret.gen_opt.post.reply_id)).doc("Set an ID so that this post can be replied to with --reply-to."),
-			 (option("-c", "--content-warning", "--cw") & value("warning", ret.gen_opt.post.content_warning)).doc("Set a content warning (or subject) for the post."),
-			 (option("-p", "--privacy", "--visibility") & visibilities).doc("Set the post's visibility.")
+			 (option("-i", "--reply-id") & value("id", ret.gen_opt.post.reply_id)).doc("Set an ID so that this post can be replied to with --reply-to.")
 			) % "generate options");
 
 	const auto queueMode = (command("queue", "q").set(ret.selected, mode::queue).doc("Manage queued favs, boosts, and posts") &
