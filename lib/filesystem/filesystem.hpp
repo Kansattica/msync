bf7405aa2e330788f89716a07c86b18ed19b5b81
Grace H <grace@hypnovir.us>
2020-04-03 12:14:27 -0700

    What if we try it with Boost?

--- lib/filesystem/filesystem.hpp
+++ lib/filesystem/filesystem.hpp
@@ -7,6 +7,9 @@ namespace fs = std::filesystem;
 #elif __has_include(<experimental/filesystem>)
 #include <experimental/filesystem>
 namespace fs = std::experimental::filesystem;
+#elif __has_include(<boost/filesystem.hpp>)
+#include <boost/filesystem.hpp>
+namespace fs = boost::filesystem;
 #else
 #error Could not find filesystem library in <filesystem> or <experimental/filesystem>
 #endif
