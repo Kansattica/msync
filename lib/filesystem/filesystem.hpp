1143b858e8ab1a9e60bf83919bf326e0ed558cb5
Grace H <grace@hypnovir.us>
2020-04-03 12:23:18 -0700

    Try using boost if no std::filesystem was found.

--- lib/filesystem/filesystem.hpp
+++ lib/filesystem/filesystem.hpp
@@ -1,15 +1,15 @@
 #ifndef _FS_HPP_
 #define _FS_HPP_
 
-#if __has_include(<filesystem>)
+#ifdef MSYNC_USE_BOOST
+#include <boost/filesystem.hpp>
+namespace fs = boost::filesystem;
+#elif __has_include(<filesystem>)
 #include <filesystem>
 namespace fs = std::filesystem;
 #elif __has_include(<experimental/filesystem>)
 #include <experimental/filesystem>
 namespace fs = std::experimental::filesystem;
-#elif __has_include(<boost/filesystem.hpp>)
-#include <boost/filesystem.hpp>
-namespace fs = boost::filesystem;
 #else
 #error Could not find filesystem library in <filesystem> or <experimental/filesystem>
 #endif
