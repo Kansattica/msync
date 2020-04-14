c1f2d2c52a69c1a1c617786bbe2eb4ab0a32deb7
Grace H <grace@hypnovir.us>
2020-04-14 14:51:00 -0700

    Always use the better realpath on Linux.

--- lib/executable_location/executable_location.cpp
+++ lib/executable_location/executable_location.cpp
@@ -1,42 +1,38 @@
 #include "executable_location.hpp"
 
-#include <string>
-#include <whereami.h>
 
 #ifdef __linux__
 #include <limits.h>
 #include <stdlib.h>
 #include <memory>
+#else
+#include <string>
+#include <whereami.h>
 #endif
 
-fs::path get_fallback_executable_folder()
+fs::path get_executable_folder()
 {
-
-	// whereami doesn't work on arm processors for some reason, so this is a fallback
+	// whereami doesn't work on arm processors for some reason, and also whereami uses PATH_MAX on Linux which you shouldn't do
+	// (see https://linux.die.net/man/3/realpath, stackoverflow answers, and blogs that say you shouldn't use PATH_MAX)
+    // so use the correct form of realpath on linux and whereami everywhere else
 #ifdef __linux__
-	// see https://linux.die.net/man/3/realpath
+
+    // this version of realpath malloc()s a buffer and returns it, so we use unique_ptr to free it automatically.
 	std::unique_ptr<char[]> full_executable_path { realpath("/proc/self/exe", NULL) };
 	fs::path to_return { full_executable_path.get() };
 	to_return.remove_filename();
 	return to_return;
 #else
-	return fs::current_path(); //probably wrong, but it's an alright fallback? Might be better to just scream and die.
-#endif
-}
-
-fs::path get_executable_folder()
-{
     const int length = wai_getModulePath(nullptr, 0, nullptr);
-
-	if (length <= 0)
-		return get_fallback_executable_folder();
 
     auto path = std::string((size_t)length + 1, '\0');
 
     int dirname_length;
     wai_getExecutablePath(&path[0], length, &dirname_length);
     return fs::path(path.begin(), path.begin() + dirname_length);
+#endif
 }
+
 
 const fs::path& executable_folder()
 {
