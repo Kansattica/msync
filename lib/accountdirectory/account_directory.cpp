7e9406d9b6c560d746f23b6ab8b9d6b9fba30d0c
Violet White <violet.white.dammit@protonmail.com>
2020-04-17 17:22:40 -0500

    Add xdg directory spec option

--- lib/accountdirectory/account_directory.cpp
+++ lib/accountdirectory/account_directory.cpp
@@ -10,13 +10,30 @@
 #include <string>
 #include <whereami.h>
 #endif
-
+#ifdef USE_XDG_DIRECTORY_SPEC
+#include <cstdlib>
+#endif
 fs::path get_executable_folder()
 {
+#ifdef USE_XDG_DIRECTORY_SPEC
+	fs::path to_return;
+	char *env_value=nullptr;
+	// If this environmental variable is set it overrides the usual ~/.config/<stuff> paths
+	if((env_value = getenv("XDG_CONFIG_HOME")) != nullptr)
+	{
+		to_return = env_value;
+	}
+	else
+	{
+		to_return = getenv("HOME");
+		to_return/=".config";
+	}
+	to_return/="msync";
+	return to_return;
 	// whereami doesn't work on arm processors for some reason, and also whereami uses PATH_MAX on Linux which you shouldn't do
 	// (see https://linux.die.net/man/3/realpath, stackoverflow answers, and blogs that say you shouldn't use PATH_MAX)
 	// so use the correct form of realpath on linux and whereami everywhere else
-#ifdef __linux__
+#elif defined(__linux__)
 	// this version of realpath malloc()s a buffer and returns it, so we use unique_ptr to free it automatically.
 	std::unique_ptr<char[], decltype(std::free)*> full_executable_path { realpath("/proc/self/exe", NULL), std::free };
 	fs::path to_return { full_executable_path.get() };
