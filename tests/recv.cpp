d871cca9f0c4b421899025599de7c54e3f1eb5f1
Grace H <Grace@hypnovir.us>
2020-04-17 18:56:24 -0700

    This might be a better way to detect to_chars

--- tests/recv.cpp
+++ tests/recv.cpp
@@ -17,7 +17,7 @@
 #include <system_error>
 #include <tuple>
 
-#if ! __cpp_lib_to_chars
+#if __cpp_lib_to_chars != 201611L
 #include <cstdio>
 #endif
 
@@ -32,7 +32,7 @@ std::string_view sv_to_chars(Number n, std::array<char, 10>& char_buf)
 	// note that this function takes a character buffer that it will clobber and returns a string view into it
 	// this is to avoid allocations and also not return pointers into memory that will be freed when the function returns.
 
-#if __cpp_lib_to_chars
+#if __cpp_lib_to_chars == 201611L
 	const auto [end, err] = std::to_chars(char_buf.data(), char_buf.data() + char_buf.size(), n);
 	if (err != std::errc()) { FAIL("You messed up with to_chars, ya dingus."); }
 	return std::string_view(char_buf.data(), end - char_buf.data());
