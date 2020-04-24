bba1fc7fac58a49513f70365293a01bc0bf66986
Grace H <grace@hypnovir.us>
2020-04-24 14:12:23 -0700

    Clarify the flow in this little preprocessor block.

--- lib/accountdirectory/account_directory.cpp
+++ lib/accountdirectory/account_directory.cpp
@@ -3,17 +3,17 @@
 #include <constants.hpp>
 
 #ifdef __linux__
-#include <limits.h>
-#include <cstdlib>
-#include <memory>
+	#include <limits.h>
+	#include <cstdlib>
+	#include <memory>
 #else
-#include <string>
-#ifdef MSYNC_USER_CONFIG
-#include <windows.h>
-#include <shlobj.h>
-#else
-#include <whereami.h>
-#endif
+	#include <string>
+	#if defined(MSYNC_USER_CONFIG)
+		#include <windows.h>
+		#include <shlobj.h>
+	#else
+		#include <whereami.h>
+	#endif
 #endif
 
 #ifdef MSYNC_USER_CONFIG
