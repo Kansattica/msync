f613985f18f4815a13929c4c3687f08eecaba860
Grace H <grace@hypnovir.us>
2020-04-27 16:34:14 -0700

    Move this to here, where it makes more sense.

--- cmake/compilerflags.cmake
+++ cmake/compilerflags.cmake
@@ -3,9 +3,3 @@ if (MSVC)
 else()
 	add_compile_options(-Wall -Wextra -pedantic)
 endif()
-
-# basically, if you compile CPR with LTO enabled, the gnu compiler emits a bunch of spurious ODR warnings for every library that includes CPR.
-# this quiets that down.
-if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-	add_link_options("-Wno-odr")
-endif()
