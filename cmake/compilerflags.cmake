a5a207f19a80639bc8c143466c50c8de1b94b324
Grace H <grace@hypnovir.us>
2020-04-27 16:30:20 -0700

    add_compile_flags has to be before targets are added.

--- cmake/compilerflags.cmake
+++ cmake/compilerflags.cmake
@@ -7,5 +7,5 @@ endif()
 # basically, if you compile CPR with LTO enabled, the gnu compiler emits a bunch of spurious ODR warnings for every library that includes CPR.
 # this quiets that down.
 if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-	set_target_properties(msync PROPERTIES LINK_FLAGS "-Wno-odr")
+	add_link_options("-Wno-odr")
 endif()
