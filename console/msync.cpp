208f686fa1f56aa0c1eba94be17747788f779cf2
Gra <grace@hypnovir.us>
2020-05-04 11:27:11 -0700

    Don't need to know how wide the path type is any more.

--- console/msync.cpp
+++ console/msync.cpp
@@ -110,7 +110,6 @@ int main(int argc, const char* argv[])
 		case mode::location:
 			plverb() << "msync is storing user data at: ";
 			pl() << as_utf8(account_directory_path());
-			pl() << "\nMy native string type is " << sizeof(fs::path::value_type) << " bytes wide!\n";
 			break;
 		case mode::yeehaw:
 			plverb() << " __________\n"
