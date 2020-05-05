857033de5c0e705b25ad0c96811d6148c9d36f86
Gra <grace@hypnovir.us>
2020-05-05 16:46:45 -0700

    OSX install build and better account_directory (#32)
    * Offer an OSX install build
    * OSX builds now respect XDG_CONFIG_HOME when MSYNC_USER_CONFIG is set.
    * Make preprocessor define route in account_directory cleaner
    * If MSYNC_USER_CONFIG is off, use realpath if available to find where the executable is, otherwise use whereami.
    * Unit test whether account_directory properly respects MSYNC_USER_CONFIG , including testing with XDG_CONFIG_HOME on platforms that support it.

--- lib/accountdirectory/account_directory.cpp
+++ lib/accountdirectory/account_directory.cpp
@@ -2,16 +2,29 @@
 
 #include <constants.hpp>
 
-#ifdef __linux__
-	#include <limits.h>
-	#include <cstdlib>
-	#include <memory>
-#else
-	#include <string>
-	#ifdef MSYNC_USER_CONFIG
+// basically, there's a number of paths we can take through here:
+
+// if MSYNC_USER_CONFIG is set:
+// - Windows has Windows-specific stuff
+// - OSX and Linux share an implementation
+// if MSYNC_USER_CONFIG is not set:
+// - Linux has linux-specific stuff
+// - Windows and OSX use whereami
+
+#ifdef MSYNC_USER_CONFIG
+	#ifdef _WIN32
 		#define WIN32_LEAN_AND_MEAN
 		#include <windows.h>
 		#include <shlobj.h>
+	#else
+		#include <cstdlib>
+	#endif
+#else
+	// realpath is available
+	#if _BSD_SOURCE || _XOPEN_SOURCE >= 500 || _XOPEN_SOURCE && _XOPEN_SOURCE_EXTENDED
+		#include <memory>
+		#include <limits.h>
+		#include <cstdlib>
 	#else
 		#include <whereami.h>
 	#endif
@@ -21,13 +34,7 @@
 
 fs::path get_user_config_folder_base()
 {
-#ifdef __linux__
-	// compliant with the XDG Base Directory Specification
-	if (const char *config_dir = getenv("XDG_CONFIG_HOME")) {
-		return config_dir;
-	}
-	return fs::path{ getenv("HOME") }.append(".config");
-#else
+#ifdef _WIN32
 	PWSTR appdata_path = nullptr;
 	auto hresult = ::SHGetKnownFolderPath(FOLDERID_LocalAppData, KF_FLAG_DEFAULT, nullptr, &appdata_path);
 	if (SUCCEEDED(hresult)) {
@@ -36,6 +43,12 @@ fs::path get_user_config_folder_base()
 		return fs::canonical(path);
 	}
 	return fs::path{};
+#else // Linux and OSX
+	// compliant with the XDG Base Directory Specification
+	if (const char *config_dir = getenv("XDG_CONFIG_HOME")) {
+		return config_dir;
+	}
+	return fs::path{ getenv("HOME") }.append(".config");
 #endif
 }
 
@@ -53,13 +66,13 @@ fs::path get_executable_folder()
 	// whereami doesn't work on arm processors for some reason, and also whereami uses PATH_MAX on Linux which you shouldn't do
 	// (see https://linux.die.net/man/3/realpath, stackoverflow answers, and blogs that say you shouldn't use PATH_MAX)
 	// so use the correct form of realpath on linux and whereami everywhere else
-#ifdef __linux__
+#if _BSD_SOURCE || _XOPEN_SOURCE >= 500 || _XOPEN_SOURCE && _XOPEN_SOURCE_EXTENDED
 	// this version of realpath malloc()s a buffer and returns it, so we use unique_ptr to free it automatically.
 	std::unique_ptr<char[], decltype(std::free)*> full_executable_path { realpath("/proc/self/exe", NULL), std::free };
 	fs::path to_return { full_executable_path.get() };
 	to_return.remove_filename();
 	return to_return;
-#else
+#else //windows and maybe osx don't have realpath
 	const int length = wai_getModulePath(nullptr, 0, nullptr);
 
 	auto path = std::string((size_t)length + 1, '\0');
@@ -74,8 +87,14 @@ fs::path get_executable_folder()
 
 #endif
 
+fs::path account_directory_path_uncached()
+{
+	return get_config_folder().append(Account_Directory);
+}
+
 const fs::path& account_directory_path()
 {
-	const static fs::path folder = get_config_folder().append(Account_Directory);
+	const static fs::path folder = account_directory_path_uncached();
 	return folder;
 }
+
--- tests/account_directory.cpp
+++ tests/account_directory.cpp
@@ -1,7 +1,15 @@
 #include <catch2/catch.hpp>
 
-#include "../lib/accountdirectory/account_directory.hpp"
 #include <constants.hpp>
+
+#if !defined(_WIN32) && defined(MSYNC_USER_CONFIG)
+	#include <cstdlib>
+	#include "test_helpers.hpp"
+	#include <string>
+	#define MSYNC_TESTING
+#endif
+
+#include "../lib/accountdirectory/account_directory.hpp"
 
 SCENARIO("account_directory_path returns the same correct path every time.")
 {
@@ -27,10 +35,11 @@ SCENARIO("account_directory_path returns the same correct path every time.")
 #ifndef MSYNC_USER_CONFIG
 		THEN("The test executable exists in the parent directory.")
 		{
+			constexpr auto filename = 
 #ifdef _WIN32
-			constexpr auto filename = "tests.exe";
+			"tests.exe";
 #else
-			constexpr auto filename = "tests";
+			"tests";
 #endif
 			REQUIRE(fs::exists(account_dir.parent_path() / filename));
 		}
@@ -45,3 +54,60 @@ SCENARIO("account_directory_path returns the same correct path every time.")
 		}
 	}
 }
+
+#ifdef MSYNC_USER_CONFIG
+SCENARIO("The account directory locator respects MSYNC_USER_CONFIG.")
+{
+	#ifdef _WIN32
+	GIVEN("The account directory path.")
+	{
+		const auto account_dir = account_directory_path();
+
+		THEN("On Windows, the path ends with AppData/Local/msync/msync_accounts.")
+		{
+			auto path_iterator = account_dir.end();
+			REQUIRE(*(--path_iterator) == Account_Directory);
+			REQUIRE(*(--path_iterator) == "msync");
+			REQUIRE(*(--path_iterator) == "Local");
+			REQUIRE(*(--path_iterator) == "AppData");
+		}
+	}
+	#else // linux and OSX
+	GIVEN("An environment where XDG_CONFIG_HOME can be checked.")
+	{
+		constexpr auto xdg_home = "XDG_CONFIG_HOME";
+		// the string that getenv returns changes when you call setenv, so save it off
+		const auto oldhome = getenv(xdg_home);
+		const std::string old_home_val = oldhome == nullptr ? "" : oldhome;
+
+		WHEN("XDG_CONFIG_HOME is unset.")
+		{
+			REQUIRE(unsetenv(xdg_home) == 0);
+			const auto account_dir = account_directory_path_uncached();
+			THEN("The path is as expected, ending in .config/msync/msync_accounts.")
+			{
+				const auto expectedpath = fs::path { getenv("HOME") } / ".config" / "msync" / Account_Directory;
+				REQUIRE(account_dir == expectedpath);
+			}
+		}
+
+		WHEN("XDG_CONFIG_HOME is set.")
+		{
+			const auto tempdir = temporary_directory();
+			REQUIRE(setenv(xdg_home, tempdir.dirname.c_str(), true) == 0);
+			const auto account_dir = account_directory_path_uncached();
+			THEN("The path is as expected.")
+			{
+				const auto expectedpath = tempdir.dirname / "msync" / Account_Directory;
+				REQUIRE(account_dir == expectedpath);
+			}
+		}
+
+		if (oldhome == nullptr)
+			unsetenv(xdg_home);
+		else
+			setenv(xdg_home, old_home_val.c_str(), true);
+	}
+	#endif
+}
+#endif
