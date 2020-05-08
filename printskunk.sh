8b26b49154f26ce1f6d761eab3641d2840ba1789
Grace H <grace@hypnovir.us>
2020-05-08 14:02:37 -0700

    If this works, I'm gonna freak.

--- printskunk.sh
+++ printskunk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 locale
-greadlink -m ./cool🦨zone
-LC_ALL=C mkdir ./cool🦨zone
-touch ./cool🦨zone
-greadlink ./cool🦨zone
+greadlink -m ./cool🤠zone
+LC_ALL=C mkdir ./cool🤠zone
+touch ./cool🤠zone
+greadlink ./cool🤠zone
