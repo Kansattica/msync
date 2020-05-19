22d5e900f563e6758fbdb2cf04995d96e574ee3e
Gra <grace@hypnovir.us>
2020-05-18 23:28:47 -0700

    Change existing language settings as little as possible for UTF-8 support. (#44)
    Basically, just try setting the code page to UTF-8 on Windows.

--- lib/fixlocale/fix_locale.cpp
+++ lib/fixlocale/fix_locale.cpp
@@ -1,15 +1,47 @@
 #include "fix_locale.hpp"
 
+#ifdef _WIN32
 #include <locale>
 #include <stdexcept>
+#include <algorithm>
+#include <cctype>
+
+bool is_utf8(std::string& localename)
+{
+	std::transform(localename.begin(), localename.end(), localename.begin(),
+		[](unsigned char c) -> unsigned char { return std::toupper(c); });
+
+	for (const auto utf8 : { "UTF-8", "UTF8", "65001" })
+	{
+		if (localename.find(utf8) != std::string::npos)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
 
 void fix_locale()
 {
 	//mostly for Windows so it interprets strings of type char as UTF-8 when constructing paths.
 	//pre-10 Windows doesn't really have UTF-8 support, so just try to do it and fail silently
-	try
+
+	// later: see if I can just add a utf-8 codecvt facet to the current locale
+	// also, write a test for this
+
+	// calling locale with an empty string is supposed to get the user's preferred locale settings
+	// it hasn't on any Windows machine I've tried, but it's worth checking, I guess?
+	if (!is_utf8(std::locale("").name()))
 	{
-		std::locale::global(std::locale("en_US.UTF-8"));
+		try
+		{
+			std::locale::global(std::locale(".65001"));
+		}
+		catch (const std::runtime_error&) {}
 	}
-	catch (const std::runtime_error&) { }
+	
 }
+#else
+void fix_locale(){}
+#endif
