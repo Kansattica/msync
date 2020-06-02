1685b43da5d573518f54238c6c245bf9d6823156
Grace H <grace@hypnovir.us>
2020-06-02 14:36:52 -0700

    msync_exception can be nothrow constructible now.

--- lib/exception/msync_exception.hpp
+++ lib/exception/msync_exception.hpp
@@ -7,13 +7,17 @@
 class msync_exception : public std::exception
 {
 public:
+	msync_exception(const char* what_arg) : noalloc_what_msg(what_arg) {}
 	msync_exception(std::string what_arg) : what_msg(what_arg) {}
 	const char* what() const noexcept
 	{
+		if (noalloc_what_msg != nullptr)
+			return noalloc_what_msg;
 		return what_msg.c_str();
 	}
 
 private:
-	std::string what_msg;
+	const char* noalloc_what_msg = nullptr;
+	const std::string what_msg;
 };
 #endif
