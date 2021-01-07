04bc735cc72637d5235938d461f510fc76e93cd6
Grace Lovelace <grace@hypnovir.us>
2021-01-06 17:22:30 -0800

    Fixing stuff up, getting accounts working.

--- msync_complete.sh
+++ msync_complete.sh
@@ -3,17 +3,21 @@
 # i think the traditional place is 
 # ~/.local/share/bash-completion/completions/ for bash
 # or you can just put it in your .bashrc or .zshrc
+# long story short, it works after doing:
+# source msync_completion.sh
 
 if [ -n "$ZSH_VERSION" ]; then autoload -U +X bashcompinit && bashcompinit; fi
 
 function _msync() {
 	local cmd="${1}"
+	local subcmd="${COMP_WORDS[1]}"
 	local word=${COMP_WORDS[COMP_CWORD]}
 	local prev=${COMP_WORDS[COMP_CWORD-1]}
 	local line=${COMP_LINE}
 
 	local accountverbose='-a --account -v --verbose';
 
+	# look at the last word to see what to propose next. This usually works, but not if the last thing was a command line option.
 	case "$prev" in
 		$cmd)
 			COMPREPLY=($( compgen -W 'new config sync gen generate q queue yeehaw location license version help' -- $word ))
@@ -30,6 +34,10 @@ function _msync() {
 			else
 				COMPREPLY=($( compgen -W "-r --retries -p --posts -m --max-requests -s --send-only -g --get-only --recv-only $accountverbose" -- $word ));
 			fi
+			return 0;
+			;;
+		'home' | 'notifications')
+			COMPREPLY=($( compgen -W 'newest oldest off' -- $word ));
 			return 0;
 			;;
 		'gen' | 'generate')
@@ -49,12 +57,27 @@ function _msync() {
 			return 0;
 			;;
 		'post' | '-f' | '--file' | '--attach' | '--attachment')
-			COMPREPLY=($( compgen -f -- $word ));
+			COMPREPLY=($( compgen -o filenames -A file -- $word ));
 			return 0;
 			;;
 		'-a' | '--account')
-			local accounts=$($cmd location)
-			COMPREPLY=($( compgen -C "ls $accounts" -- $word ));
+			local accounts=$(ls "$(./msync location)")
+			local lowercased=$(echo "$accounts" | awk '{print tolower($0)}')
+			COMPREPLY=($( compgen -W "$accounts $lowercased" -- $word ));
+			return 0;
+			;;
+	esac
+
+	# if the first one failed, check the second argument- the 'subcommand'
+	# right now, there's some duplication of effort- if this came first, we could get rid of the weird if statement in the sync for the last one
+	# but the $last switch statement provides autocompletion for the visibility settings, and so that has to come first.
+	case "$subcmd" in
+		'gen' | 'generate')
+			COMPREPLY=($( compgen -W '-d --description -f --file --attach --attachment -o --output -r --reply-to -i --reply-id -c --content-warning --cw -b --body --content -p --privacy --visibility' -- $word ));
+			return 0;
+			;;
+		'sync')
+			COMPREPLY=($( compgen -W "-r --retries -p --posts -m --max-requests -s --send-only -g --get-only --recv-only $accountverbose" -- $word ));
 			return 0;
 			;;
 	esac
