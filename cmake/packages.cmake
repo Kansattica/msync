5ab894c39a8870568b43cd2b5a28fe7157ca3cd6
Grace L <Grace@hypnovir.us>
2023-10-12 21:55:35 -0700

    Quiet warning about zip file timestamps.

--- cmake/packages.cmake
+++ cmake/packages.cmake
@@ -1,3 +1,5 @@
+cmake_policy(SET CMP0135 NEW) #fix warning about zip file timestamps
+
 include(FetchContent)
 
 message(STATUS "Downloading nlohmann json...")
@@ -5,7 +7,6 @@ FetchContent_Declare(
 	njson
 	URL https://github.com/nlohmann/json/releases/download/v3.10.5/include.zip
 	URL_HASH SHA256=b94997df68856753b72f0d7a3703b7d484d4745c567f3584ef97c96c25a5798e
-	DOWNLOAD_EXTRACT_TIMESTAMP true
 	)
 
 #FetchContent_MakeAvailable(json) Not available in cmake 13
